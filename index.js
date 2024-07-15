863fd250f7a3876c99500cea235a208dd0d82c55
sowmik-sec <sowmik.sec@gmail.com>
2024-07-15 11:12:31 +0600

    create post request api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const users = [
   { id: 1, name: "Sabana", email: "sabana@gmail.com" },
@@ -17,6 +18,14 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
